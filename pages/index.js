2853a3bdf49ca42248fdb806dc605c20042f3fc8
Marvin Lee <marvinlee.github@gmail.com>
2022-07-12 21:48:04 -0700

    Created and cleared out example files from new NextJS app

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>The Pantry Cabinet</title>
+        <meta name="description" content="A Pantry Inventory Site Built With NextJS and Tailwind" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Under Construction
+        </h1>
+      </main>
+    </>
+  );
+}
