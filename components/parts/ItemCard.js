ee7ac8f3cfbf2f00954e06cb407f1dcabac07886
Marvin Lee <marvinlee.github@gmail.com>
2022-07-19 14:04:49 -0700

    Basic card created with Tailwind

--- components/parts/ItemCard.js
+++ components/parts/ItemCard.js
@@ -1,7 +1,16 @@
+import cakeImage from "../../images/cake.jpg";
+
 export default function ItemCard() {
 	return (
-		<div className="mx-5 border border-black">
-			Hi
+		<div className="mx-5 p-2 border-4 rounded-lg border-transparent transition-[border-color] duration-200 hover:border-neutral-200 w-60 aspect-[5/6] overflow-hidden">
+			<div className="-mx-2 -mt-2 w-100">
+				<img
+					className="w-100 aspect-[4/3] object-cover"
+					src={cakeImage}
+					alt="Sample Image"
+				/>
+			</div>
+			Cake
 		</div>
 	);
 }
