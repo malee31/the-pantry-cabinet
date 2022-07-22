70adf7a6968173f836630fd84025c00cf4fc4a5e
Marvin Lee <marvinlee.github@gmail.com>
2022-07-22 14:44:51 -0700

    Added props to ItemCard and created variant for it in Storybook

--- components/parts/ItemCard.js
+++ components/parts/ItemCard.js
@@ -2,14 +2,25 @@ import cakeImage from "../../public/images/cake.jpg";
 import ItemCounter from "./ItemCounter";
 import Image from "next/image";
 
-export default function ItemCard() {
+export default function ItemCard(props) {
+	const {
+		imageSrc = cakeImage,
+		label = "Cake",
+		caption = "A very tasty cake left in the freezer for way too long. Definitely severely freezer burned... But it should still be tasty... right?",
+		defaultAmount = 0,
+		...args
+	} = props;
+
 	return (
-		<div className="flex flex-col mx-4 my-2 p-2 border-4 rounded-lg border-neutral-100 transition-[border-color] duration-200 hover:border-neutral-200 h-72 aspect-[5/6] overflow-hidden">
-			<div className="relative -mx-2 -mt-2 p-2 box-content w-full aspect-[4/3] bg-black">
+		<div
+			{...args}
+			className="flex flex-col mx-4 my-2 p-2 border-4 rounded-lg border-neutral-100 transition-[border-color] duration-200 hover:border-neutral-200 h-72 aspect-[5/6] overflow-hidden"
+		>
+			<div className="relative flex-shrink-0 -mx-2 -mt-2 p-2 box-content w-full aspect-[4/3] bg-black">
 				<div className="absolute w-full h-full -m-2 ">
 					<Image
 						className="w-100 aspect-[4/3] object-cover object-center"
-						src={cakeImage}
+						src={imageSrc}
 						alt="Sample Image"
 						layout="fill"
 					/>
@@ -18,14 +29,14 @@ export default function ItemCard() {
 			<div className="flex flex-col min-h-0">
 				<div>
 					<span className="-mr-1 float-right">
-						<ItemCounter defaultAmount={1}/>
+						<ItemCounter defaultAmount={defaultAmount}/>
 					</span>
 					<h5 className="h-8 text-xl align-middle leading-8">
-						Cake
+						{label}
 					</h5>
 				</div>
 				<p className="flex-1 -m-2 mt-0 p-2 pt-0 min-h-0 overflow-auto">
-					A very tasty cake left in the freezer for way too long. Definitely severely freezer burned... But it should still be tasty... right?
+					{caption}
 				</p>
 			</div>
 		</div>
--- stories/parts/ItemCard.stories.js
+++ stories/parts/ItemCard.stories.js
@@ -1,6 +1,22 @@
 import ItemCard from "../../components/parts/ItemCard";
+import cakeImage from "../../public/images/cake.jpg";
 
-export const Base = () => <ItemCard/>;
+const Template = (args) => <ItemCard {...args}/>;
+export const Base = Template.bind({});
+Base.args = {
+	imgSrc: cakeImage,
+	label: "Cake",
+	caption: "A very tasty cake left in the freezer for way too long. Definitely severely freezer burned... But it should still be tasty... right?",
+	defaultAmount: 0
+}
+
+export const Short_Text = Template.bind({});
+Short_Text.args = {
+	imgSrc: cakeImage,
+	label: "Cake",
+	caption: "Good Cake",
+	defaultAmount: 0
+}
 
 export default {
 	title: "Parts/Item Card",
