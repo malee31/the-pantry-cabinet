9bbb48a3355dc6682a92056a509f181cfc2b9e8d
Marvin Lee <marvinlee.github@gmail.com>
2022-07-25 12:23:05 -0700

    Converted index.js to a simple wrapper for InventoryScreen.js

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,5 @@
-import Head from "next/head";
-import Navbar from "../components/parts/Navbar";
-import Sidebar from "../components/sections/Sidebar";
-import ItemGrid from "../components/units/ItemGrid";
-import { useState } from "react";
-import ItemCard from "../components/parts/ItemCard";
+import InventoryScreen from "../components/entities/InventoryScreen";
 
 export default function Home() {
-	const [showSampleItems, setShowSampleItems] = useState(false);
-
-	return (
-		<>
-			<Head>
-				<title>The Pantry Cabinet</title>
-				<meta name="description" content="A Pantry Inventory Site Built With NextJS and Tailwind"/>
-				<link rel="icon" href="/favicon.ico"/>
-			</Head>
-
-			<main className="w-screen h-screen grid" style={{ gridTemplate: "min-content/min-content 1fr" }}>
-				<Navbar className="col-span-2"/>
-				<Sidebar/>
-				{
-					showSampleItems ? (
-						<ItemGrid>
-							{
-								Array(20)
-									.fill(0)
-									.map((item, index) => (
-										<ItemCard key={index}/>
-									))
-							}
-						</ItemGrid>
-					) : <ItemGrid noItemsLabel="No Items Available. Click to Show Demo" onClick={() => setShowSampleItems(true)}/>
-				}
-			</main>
-		</>
-	);
+	return (<InventoryScreen/>)
 }
