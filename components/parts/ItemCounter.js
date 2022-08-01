aaf3222c2cb63941cc68fe11b44f22b5c49f6ac0
Marvin Lee <marvinlee.github@gmail.com>
2022-07-31 21:40:20 -0700

    Improved counter when decrementing or typing into input

--- components/parts/ItemCounter.js
+++ components/parts/ItemCounter.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { MinusSmIcon, PlusSmIcon } from "@heroicons/react/solid";
 import { classNameMerge } from "../../utils";
+import { Nu } from "../../public/storybook/947.d1b00fb4.iframe.bundle";
 
 /**
  * The counter for the number of an item there is with controls to add or subtract
@@ -16,6 +17,21 @@ export default function ItemCounter(props) {
 		...args
 	} = props;
 	const [count, setCount] = useState(defaultAmount);
+	const [inputVal, setInputVal] = useState(null);
+
+	const onInput = e => {
+		const input = e.currentTarget.value;
+		const filteredInput = input.replace(/[^0-9]/g, "");
+		console.log(filteredInput)
+		setInputVal(filteredInput);
+	};
+
+	const onBlur = () => {
+		const inputNumber = Number(inputVal);
+		const newCount = (!inputVal || isNaN(inputNumber)) ? count : inputNumber;
+		setCount(newCount);
+		setInputVal(null);
+	};
 
 	return (
 		<div
@@ -26,17 +42,19 @@ export default function ItemCounter(props) {
 			<MinusSmIcon
 				className="h-full flex-grow-0 aspect-square shrink-0 rounded-full bg-blue-400 cursor-pointer select-none"
 				role="button"
-				onClick={() => setCount(Math.max(0, count - 1))}
+				onClick={() => count > 0 && setCount(count - 1)}
 			/>
 			<input
 				className="w-9 h-full px-1 flex-grow-0 text-center"
-				value={count}
-				onChange={e => setCount(Number(e.currentTarget.value) || count)}
+				type="text"
+				value={typeof inputVal === "string" ? inputVal : count}
+				onInput={onInput}
+				onBlur={onBlur}
 			/>
 			<PlusSmIcon
 				className="h-full flex-grow-0 aspect-square shrink-0 rounded-full bg-blue-400 cursor-pointer select-none"
 				role="button"
-				onClick={() => setCount(Math.max(0, count + 1))}
+				onClick={() => setCount(count + 1)}
 			/>
 		</div>
 	);
